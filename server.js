fd9a6c0cd5946b94a9941a0ede93788489631741
joberaco <bernardoRamirez@outlook.com>
2022-05-13 14:50:28 -0500

    init

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ app.use(express.static(__dirname + '/dist/todo-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/todo-app/index.html'));});
+
 app.listen(process.env.PORT || 8080);
